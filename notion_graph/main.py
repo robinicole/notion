dbf83a868151261c2afad8f2f3748012a2d37283
Robin Nicole <robin@192.168.1.157>
2022-07-12 11:29:15 +0100

    notion_api: initial commit

---
+++ notion_graph/main.py
@@ -0,0 +1,71 @@
+import requests 
+import networkx as nx
+from pyvis.network import Network
+import sys
+
+class NotionDatabase:
+    """
+    High level interface on top of a notion database
+    """
+    def __init__(self,token: str, database_id: str):
+        self._data = self.download_data(token, database_id)
+    
+    def download_data(self, token: str, database_id: str) -> dict:
+        headers = {
+        "Authorization": f'Bearer {token}',
+        "Notion-Version": "2022-02-22",
+        "Content-Type": "application/json"
+        }    
+        resp = requests.post(f'https://api.notion.com/v1/databases/{database_id}/query', headers=headers)
+        resp = resp.json()
+        return resp['results']
+
+    @staticmethod
+    def get_title(result: dict) -> str:
+        return result['properties']['Name']['title'][0]['plain_text']
+
+    @staticmethod
+    def get_id(result: dict) -> str:
+        return result['id']
+    
+    @staticmethod
+    def get_children(result: str) -> list:
+        return [el['id'] for el in result['properties']['Children']['relation']]
+
+    @property
+    def id_to_title(self) -> dict:
+        return {row['id']: row['title'] for row in self.data}
+
+    @property
+    def data(self) -> dict:
+        return [{
+            'title': self.get_title(result),
+            'id': self.get_id(result),
+            'children': self.get_children(result),
+        } for result in self._data]
+
+    @property
+    def graph(self) -> nx.DiGraph:
+        edges = []
+        for row in self.data:
+            for children in row['children']:
+                edges.append((self.id_to_title[row['id']], self.id_to_title[children]))
+        return nx.DiGraph(edges)
+
+    def show_graph(self, output="example.html", **kwargs):
+        net = Network(notebook=True, directed=True)
+        net.from_nx(self.graph)
+        net.force_atlas_2based(**kwargs)
+        return net.show(output)
+
+if __name__ == '__main__':
+    try:
+        token = sys.argv[1]
+        database_id = sys.argv[2]
+        output = sys.argv[3]
+        notion_db_perso = NotionDatabase(token=token, database_id=database_id)
+        notion_db_perso.show_graph(output=output)
+        print(f"results in {output}")
+    except Exception as e:
+        print(f"Exception raised {e}") 
+        print("Did you use this script with poetry run python -m notion_graph.main {secret_token} {database_id} {output}")
